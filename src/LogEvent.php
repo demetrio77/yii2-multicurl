239f1c24e26887a1f70b8a7ab3599cdcec89a403
Dmitry Karpov <demetrio77@gmail.com>
2020-01-18 21:32:10 +0300

    Log type added

--- src/LogEvent.php
+++ src/LogEvent.php
@@ -7,8 +7,9 @@ class LogEvent extends Event
 {
     const NAME = 'parsing.log.event';
     
-    const TYPE_MESSAGE = 'message';
-    const TYPE_ERROR = 'error';
+    const TYPE_MESSAGE = 1;
+    const TYPE_END = 3;
+    const TYPE_ERROR = 2;
     
     public $message;
     public $type = self::TYPE_MESSAGE;
